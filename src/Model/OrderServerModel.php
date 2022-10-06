bf9142acfe5a7b4c817df5b242cbb6060f4363a5
ii02735 <yadallee.bilaal@outlook.fr>
2022-10-06 13:03:30 +0200

    [MODEL] Class created for OrderServerType form

--- src/Model/OrderServerModel.php
+++ src/Model/OrderServerModel.php
@@ -2,10 +2,97 @@
 
 namespace App\Model;
 
+use App\Entity\Option;
+use App\Entity\Product;
 use App\Entity\Server;
 
-class OrderFilling
+use App\Validator\OrderServer as OrderServerAssert;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @Assert\GroupSequence({"First","OrderServerModel","After"})
+ * @OrderServerAssert\SoftwareCompatibility
+ */
+class OrderServerModel
 {
+    /**
+     * @var Server
+     * @Assert\NotNull(message = "Vous devez choisir un serveur", groups={"First"})
+     */
     private Server $server;
+
+    /**
+     * @Assert\Sequentially({
+     *      @Assert\Count(max = 5, maxMessage = "Vous pouvez choisir jusqu'à 5 logiciels seulement"),
+     *      @OrderServerAssert\UniqueWebServerSoftware(groups={"After"})
+     * })
+     * @var Product[] $softwares
+     */
+    private array $softwares = [];
+
+    /**
+     * @var Product[] $options
+     */
+    private array $options = [];
+
+    /**
+     * @return Server
+     */
+    public function getServer(): Server
+    {
+        return $this->server;
+    }
+
+    /**
+     * @param Server $server
+     * @return OrderServerModel
+     */
+    public function setServer(Server $server): self
+    {
+        $this->server = $server;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSoftwares(): array
+    {
+        return $this->softwares;
+    }
+
+    /**
+     * @param array $softwares
+     * @return OrderServerModel
+     */
+    public function setSoftwares(array $softwares): self
+    {
+        $this->softwares = $softwares;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param array $options
+     * @return OrderServerModel
+     */
+    public function setOptions(array $options): self
+    {
+        $this->options = $options;
+        return $this;
+    }
+
+    public function getServerType(): string
+    {
+        return $this->server->getServerType();
+    }
+
 
 }
